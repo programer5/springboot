b93206349cea5ae236a9008642eab52cd083fa57
minseo23 <neverdie4757@gmail.com>
2021-08-16 14:47:51 +0900

    CodeDeploy 로그 확인

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -8,7 +8,7 @@ cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
 echo "> 현재 구동 중인 애플리케이션 pid 확인"
 
-CURRENT_PID=$(pgrep -fi springboot | grep jar | awk '{print $1}')
+CURRENT_PID=$(pgrep -f1 springboot | grep jar | awk '{print $1}')
 
 echo "현재 구동 중인 애플리케이션 pid: $CURRENT_PID"
 
